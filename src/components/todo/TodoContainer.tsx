3de72c684c27ebfe583a7a9da9e6dc3f882706dc
ijaz35 <ijaz.hossain1@gmail.com>
2024-01-20 13:14:27 +0600

    rtk query addede

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -2,9 +2,18 @@ import { useAppSelector } from "@/redux/hook";
 import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 import TodoFilter from "./TodoFilter";
+import { useGetTodosQuery } from "@/redux/api/api";
 
 const TodoContainer = () => {
-  const { todos } = useAppSelector((state) => state.todos);
+  //* data retrive from local state
+  // const { todos } = useAppSelector((state) => state.todos);
+
+  // * From server
+  const { data: todos, isLoading, isError } = useGetTodosQuery(undefined);
+  console.log(todos);
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
   return (
     <div>
       <div className="flex justify-between">
@@ -16,7 +25,7 @@ const TodoContainer = () => {
           <p>There is no task pending</p> 
         </div>*/}
         <div className="bg-white p-5 w-full rounded-lg space-y-3">
-          {todos.map((item) => (
+          {todos?.data?.map((item) => (
             <TodoCard {...item} />
           ))}
         </div>
